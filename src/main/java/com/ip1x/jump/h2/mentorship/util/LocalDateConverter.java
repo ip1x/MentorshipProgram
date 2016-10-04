3da86b3e816e6927f6a9366169cd246b1b19bb86
Maxym_Rudevych <mrudevich96@gmail.com>
2016-10-04 12:09:53 +0300

    add custom message for date parse exception

--- src/main/java/com/ip1x/jump/h2/mentorship/util/LocalDateConverter.java
+++ src/main/java/com/ip1x/jump/h2/mentorship/util/LocalDateConverter.java
@@ -3,9 +3,14 @@ package com.ip1x.jump.h2.mentorship.util;
 import org.springframework.core.convert.converter.Converter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class LocalDateConverter implements Converter<String, LocalDate>{
     public LocalDate convert(String s) {
-        return LocalDate.parse(s);
+        try {
+            return LocalDate.parse(s);
+        }catch (DateTimeParseException e){
+            throw new RuntimeException("Please set correct date");
+        }
     }
 }
